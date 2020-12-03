d2d18ec15812bbef3ba530e1d213c01c43075f24
afkan_guseynov <af.kan@bk.ru>
2020-12-03 19:10:09 +0500

    4. Напишите программу на Java, которая читает целое число от    0 до 1000 и добавляет все цифры в целое число.

---
+++ dataType/src/Exercise4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Exercise4 {
+    /*Напишите программу на Java для преобразования минут в количество лет и дней.*/
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double minutsInYear = 60 * 24 * 365;
+        System.out.println("input the number of minutes: ");
+        double min = input.nextDouble();
+        long years = (long)(min / minutsInYear);
+        int days  = (int)(min/60/24)/365;
+        System.out.println((int)min + "minutes is appoximately" + years  + " years" + days + " days" );
+
+
+    }
+}
